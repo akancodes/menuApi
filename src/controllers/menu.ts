3ea2fb2bf82fb37bb05022302174b1f770f74dec
akancodes <akancodes@gmail.com>
2022-03-21 14:35:18 +0300

    added PUT and DELETE routes

--- src/controllers/menu.ts
+++ src/controllers/menu.ts
@@ -66,10 +66,77 @@ const createMenu = async (req: Request, res: Response, next: NextFunction) => {
   });
 };
 
+const deleteMenu = async (req: Request, res: Response, next: NextFunction) => {
+  const { id } = req.params;
+
+  try {
+    const menu = await Menu.findById(id);
+
+    // Check if menu not exists
+    if (!menu) {
+      return res.status(404).json({
+        message: "Menu not found",
+      });
+    }
+
+    // TODO: User validation
+
+    // Delete menu
+    await menu.remove();
+    res.status(200).json({
+      message: "Menu deleted successfully!",
+    });
+  } catch (error: any) {
+    res.status(404).json({
+      message: "Failed to delete menu",
+    });
+  }
+};
+
+const updateMenu = async (req: Request, res: Response, next: NextFunction) => {
+  const { id } = req.params;
+  const { title, image } = req.body;
+
+  try {
+    const menu = await Menu.findById(id);
+
+    // Check if menu not exists
+    if (!menu) {
+      return res.status(404).json({
+        message: "Menu doesn't exist!",
+      });
+    }
+
+    // TODO: User validation
+
+    // Check fields are given
+    if (!title || !image) {
+      return res.status(400).json({
+        message: "Please provide all required fields",
+      });
+    }
+
+    // Update menu
+    menu.title = title;
+    menu.image = image;
+    await menu.save();
+    res.status(200).json({
+      message: "Menu updated successfully!",
+      menu,
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      message: "An error occured!",
+    });
+  }
+};
+
 const menuController = {
   getMenu,
   getMenuById,
   createMenu,
+  deleteMenu,
+  updateMenu,
 };
 
 export default menuController;
--- src/routes/menu.ts
+++ src/routes/menu.ts
@@ -12,4 +12,10 @@ router.get("/:id", controllers.menu.getMenuById);
 // * POST /api/menu
 router.post("/", controllers.menu.createMenu);
 
+// * PUT /api/menu/:id
+router.put("/:id", controllers.menu.updateMenu);
+
+// * DELETE /api/menu/:id
+router.delete("/:id", controllers.menu.deleteMenu);
+
 export default router;
