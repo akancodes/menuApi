bbdedd78fa0f6cb0f7cf96a22ef225b70fb73650
akancodes <akancodes@gmail.com>
2022-03-21 11:13:36 +0300

    connected to db

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,23 @@ import express from "express";
 import helmet from "helmet";
 import cors from "cors";
 import "dotenv/config";
+import connectToDatabase from "./helpers/database";
 
 const app = express();
 
+app.use(express.json());
 app.use(helmet());
 app.use(cors());
+
+if (!process.env.DB_USER || !process.env.DB_PASS || !process.env.DB_NAME) {
+  throw new Error("Missing DB_USER, DB_PASSWORD or DB_NAME in .env file");
+}
+
+connectToDatabase(
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  process.env.DB_NAME
+);
 
 app.listen(process.env.PORT || 8080, () => {
   console.log("Server is running");
